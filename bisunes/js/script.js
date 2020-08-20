14659adb59b332bb18be5bc4cd26a5ed712df71e
kirillat6 <58559854+kirillat6@users.noreply.github.com>
2020-08-20 20:14:13 +0300

    Add files via upload

---
+++ bisunes/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.decision__slider').slick({
+        speed: 300,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrows/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrows/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows:false,
+                dots: true
+              }
+            }
+        ]
+      });
+
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      });
+  
+      $("a[href^='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      });
+    });
